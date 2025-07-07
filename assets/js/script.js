7aa000e6ec2d175201faeb3c8e53ec64693cd178
DamianoMura <mura.damiano14@gmail.com>
2025-07-07 03:50:45 +0200

    i already finished it ahah no need for loads commits

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+console.log(document.getElementsByClassName("box"));
+//console.log(document.getElementsByClassName("box").length)
+function swapColors(){
+for(let x=0 ; x<document.getElementsByClassName("box").length ; x++){
+  let dump="";
+  console.log(x)
+  console.log("the actual property initially is--> document.getElementsByClassName('box').item("+x+").className = "+document.getElementsByClassName("box").item(x).className)
+  if(document.getElementsByClassName("box").item(x).classList[1]=="bg-success"){
+    
+    dump=document.getElementsByClassName("box").item(x).className;
+    dump=dump.replace("bg-success","bg-danger");
+   
+  }
+  else{
+    dump=document.getElementsByClassName("box").item(x).className;
+    dump=dump.replace("bg-danger","bg-success")
+   
+  }
+  document.getElementsByClassName("box").item(x).className=dump;
+  console.log("swapping the class name with a string method: replace will become --> document.getElementsByClassName('box').item("+x+").className = "+document.getElementsByClassName("box").item(x).className)
+}
+
+}
